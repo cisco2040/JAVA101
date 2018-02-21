f7ea3b00e63967316fa6cace4225b74d1be81ec9
Victor-H-Cortes <36414082+Victor-H-Cortes@users.noreply.github.com>
2018-02-20 23:27:44 -0600

    Application files from Maven-by-Example  http://books.sonatype.com/mvnex-book/reference/index.html

---
+++ code/VHCT/Maven-by-Example/simple-weather/src/main/java/org/sonatype/mavenbook/weather/YahooRetriever.java
@@ -0,0 +1,20 @@
+package org.sonatype.mavenbook.weather;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.log4j.Logger;
+
+public class YahooRetriever {
+
+	private static Logger log = Logger.getLogger(YahooRetriever.class);
+
+	public InputStream retrieve(String zipcode) throws Exception {
+		log.info("Retrieving Weather Data");
+		// String url = "https://query.yahooapis.com/forcastrss?p=" + zipcode; // old yahoo api has been deprecated
+		String url = "https://query.yahooapis.com/v1/public/yql?format=xml&q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%20%27" + zipcode + "%27%20and%20u%20%3D%20%27C%27%3B";
+		URLConnection conn = new URL(url).openConnection();
+		return conn.getInputStream();
+	}
+}
