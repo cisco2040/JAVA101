a2ddf3caca7a4e8fbb627a59ef051f955347ef44
Victor-H-Cortes <36414082+Victor-H-Cortes@users.noreply.github.com>
2018-02-21 21:28:58 -0600

    simple mutli module project from maven-by-example book.

---
+++ code/VHCT/Maven-by-Example/multi/simple-parent/simple-weather/src/main/java/org/sonatype/mavenbook/weather/Main.java
@@ -0,0 +1,39 @@
+package org.sonatype.mavenbook.weather;
+
+import java.io.InputStream;
+
+import org.apache.log4j.PropertyConfigurator;
+
+public class Main {
+	public static void main (String[] args) throws Exception {
+		// Configure Log4J
+		PropertyConfigurator.configure(Main.class.getClassLoader().getResource("log4j.properties"));
+
+		// Read the zip code from the command line
+		// (if none suppplied, use 60202)
+		String zipcode = "12784107"; // not a zipcode bot WOEID as yahoo weather API was updated
+		try {
+			zipcode = args[0];
+		} catch (Exception e) {}
+
+		// Start the program
+		new Main(zipcode).start();
+	}
+
+	private String zip;
+
+	public Main (String zip) {
+		this.zip = zip;
+	}
+
+	public void start() throws Exception {
+		// Retrieve Data
+		InputStream dataIn = new YahooRetriever().retrieve(zip);
+
+		// Parse Data
+		Weather weather = new YahooParser().parse(dataIn);
+
+		// Formar (Print) Data
+		System.out.print(new WeatherFormatter().format(weather));
+	}
+}
