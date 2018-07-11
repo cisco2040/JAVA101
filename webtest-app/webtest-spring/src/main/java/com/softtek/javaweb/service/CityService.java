94d04e6c0fff5019d2801bee41bc51118fe1fc66
Victor C <36414082+Victor-H-Cortes@users.noreply.github.com>
2018-07-11 08:48:06 -0500

    adding loggers

--- webtest-app/webtest-spring/src/main/java/com/softtek/javaweb/service/CityService.java
+++ webtest-app/webtest-spring/src/main/java/com/softtek/javaweb/service/CityService.java
@@ -2,23 +2,32 @@ package com.softtek.javaweb.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.softtek.javaweb.domain.model.Cart;
 import com.softtek.javaweb.domain.model.City;
 import com.softtek.javaweb.repository.MyRepository;
 
 @Service
 public class CityService {
 	
+	public static final Logger LOGGER = LoggerFactory.getLogger(CartService.class);
+
 	@Autowired
 	private MyRepository<City,Long> cityRepository;
 	
 	public List<City> getList() {
-		return this.cityRepository.list();
+		List<City> cities = this.cityRepository.list();
+		LOGGER.info("## City List Obtained: {}", cities);
+		return cities;
 	}
 
 	public City getOne(final Long id) {
+		City city = this.cityRepository.getOne(id);
+		LOGGER.info("## City Obtained: {}", city);
 		return this.cityRepository.getOne(id);
 	}
 }
