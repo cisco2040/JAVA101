ec7696a7db0eba03cd45b89a6d50e7d777980b6b
VictorCortes <victor.cortes@softtek.com>
2019-11-27 08:50:12 -0600

    updated db init script to select db after creations. Fixed some typos in ppt.

--- SQL-master/Exercises/init-sampleDB.sql
+++ SQL-master/Exercises/init-sampleDB.sql
@@ -1,9 +1,11 @@
 /* Crear la DB, usuario y asignacion de privilegios con usuario root */
 CREATE database myDB;
+
 CREATE USER myuser@'%' IDENTIFIED BY 'mypassword';
 
 GRANT ALL ON myDB.* TO myuser@'%';
 
+USE myDB;
 
 /******** Luego conectar con el usuario 'dbuser' para crear las tablas *******/
 /* jdbc:mysql://localhost:3306/sampleDB */
